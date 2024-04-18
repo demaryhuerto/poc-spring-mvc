b29166d07278bff2fd306a3ccb48bc1500038840
Gabriel Jiménez Peña <gjimenez@nextdigital.es>
2024-04-18 12:04:26 +0200

    initial spring mvc application

---
+++ src/main/java/es/nextdigital/training/spring/mvc/PocMvcApplication.java
@@ -0,0 +1,13 @@
+package es.nextdigital.training.spring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/nextdigital/training/spring/mvc/PocMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package es.nextdigital.training.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
