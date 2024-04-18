0ee3360b6816bdcc7784b942460df9fbad12459a
Gabriel Jiménez Peña <gjimenez@nextdigital.es>
2024-04-18 12:18:58 +0200

    add User entity, repository and controller

---
+++ src/main/java/es/nextdigital/training/spring/mvc/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package es.nextdigital.training.spring.mvc.repository;
+
+import es.nextdigital.training.spring.mvc.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+  Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/es/nextdigital/training/spring/mvc/rest/UserController.java
@@ -0,0 +1,29 @@
+package es.nextdigital.training.spring.mvc.rest;
+
+import es.nextdigital.training.spring.mvc.domain.User;
+import es.nextdigital.training.spring.mvc.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+  private final UserRepository userRepository;
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public User getUserById(@PathVariable("id") String userId) {
+    return userRepository.findById(userId).orElseThrow();
+  }
+
+  @GetMapping("/email/{email}")
+  @ResponseStatus(HttpStatus.OK)
+  public Optional<User> getUserByEmail(@PathVariable("email") String email) {
+    return userRepository.findByEmail(email);
+  }
+}
