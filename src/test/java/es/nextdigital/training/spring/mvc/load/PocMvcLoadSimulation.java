7f472d3cc27c3fa14d55f0ec516f4745d59c83bd
Gabriel Jiménez Peña <gjimenez@nextdigital.es>
2024-04-20 00:19:44 +0200

    add gatling and load test

---
+++ src/test/java/es/nextdigital/training/spring/mvc/load/PocMvcLoadSimulation.java
@@ -0,0 +1,28 @@
+package es.nextdigital.training.spring.mvc.load;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public class PocMvcLoadSimulation extends Simulation {
+  HttpProtocolBuilder httpProtocol =
+    http.baseUrl("http://localhost:8080")
+      .acceptHeader("application/json")
+      .contentTypeHeader("application/json");
+
+  ScenarioBuilder myFirstScenario = scenario("POC MVC Scenario")
+    .exec(http("Request 1")
+      .get("/user/email/taylorhancock@andrews.biz"));
+
+  {
+    setUp(
+      myFirstScenario.injectOpen(
+        rampUsers(50).during(10),
+        constantUsersPerSec(50).during(20)
+      )
+    ).protocols(httpProtocol);
+  }
+}
